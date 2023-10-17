e4f4c95e93cdff43c3a6ac59e9c7bb16433f92f1
web3-king <jacobroberto950212@gmail.com>
2023-10-17 23:03:45 +0300

    add file upload section

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -30,7 +30,7 @@ const showingNavigationDropdown = ref(false);
                             <!-- Navigation Links -->
                             <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                                 <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                                    Dashboard
+                                    Figma Wizard
                                 </NavLink>
                             </div>
                         </div>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -43,8 +43,8 @@ defineProps<{
             
             <div class="mt-16">
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
-                    <a
-                        href="https://laravel.com/docs"
+                    <Link
+                        :href="route('figma2html')"
                         class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500"
                     >
                         <div>
@@ -87,10 +87,10 @@ defineProps<{
                                 d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
                             />
                         </svg>
-                    </a>
+                    </Link>
 
-                    <a
-                        href="https://laracasts.com"
+                    <Link
+                        :href="route('html2figma')"
                         class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500"
                     >
                         <div>
@@ -132,10 +132,10 @@ defineProps<{
                                 d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
                             />
                         </svg>
-                    </a>
+                    </Link>
 
-                    <a
-                        href="https://laravel-news.com"
+                    <Link
+                        :href="route('figma2laravel')"
                         class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500"
                     >
                         <div>
@@ -178,16 +178,16 @@ defineProps<{
                                 d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
                             />
                         </svg>
-                    </a>
-
-                    <div
+                    </Link>
+                    <Link
+                        :href="route('figma2other')"
                         class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500"
                     >
                         <div>
                             <div
                                 class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full"
                             >
-                                <svg
+                            <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     fill="none"
                                     viewBox="0 0 24 24"
@@ -205,11 +205,26 @@ defineProps<{
                             <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Figma to React, Vue, Flutter</h2>
 
                             <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                Seamlessly translate your Figma designs into React, Vue, or Flutter code, enabling you to build interactive and dynamic user interfaces with ease.
+                                Transform your Figma designs into a fully functional Laravel application, leveraging the power of PHP and Laravel's robust framework. 
 
                             </p>
                         </div>
-                    </div>
+
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+                            />
+                        </svg>
+                    </Link>
+                    
                 </div>
             </div>
 
