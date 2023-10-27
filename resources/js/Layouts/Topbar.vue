18038f160997ba44cd608a668bc8062c37339571
Yuriy Leve <yuriyleve@gmail.com>
2023-10-27 15:59:59 -0300

    reset previous topbar

---
+++ resources/js/Layouts/Topbar.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue';
+import NavLink from '@/Components/NavLink.vue';
+import TopbarData from './Topbar.js';
+
+const TopbarItems = ref(TopbarData);
+</script>
+
+<template>
+    <NavLink v-for="item in TopbarItems" :key="item.route" :href="route(item.route)" :active="route().current(item.route)">
+        {{ item.name }}
+    </NavLink>
+</template>
